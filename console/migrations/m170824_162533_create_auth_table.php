3e132845930e953e1763fb10f2c26809c58cd4c9
Zharkov Andrey <zharkov.php@gmail.com>
2017-08-24 21:24:42 +0300

    auto feature-auth facebook

---
+++ console/migrations/m170824_162533_create_auth_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `auth`.
+ */
+class m170824_162533_create_auth_table extends Migration
+
+{
+    public function up()
+{
+
+    $this->createTable('auth', [
+        'id' => $this->primaryKey(),
+        'user_id' => $this->integer()->notNull(),
+        'source' => $this->string()->notNull(),
+        'source_id' => $this->string()->notNull(),
+    ]);
+
+    $this->addForeignKey('fk-auth-user_id-user-id', 'auth', 'user_id', 'user', 'id', 'CASCADE', 'CASCADE');
+}
+
+    public function down()
+{
+    $this->dropTable('auth');
+
+}
+}
